89047c4b9db0baa4f4e4bcf3e5cb6716c6e08bd5
LUXIANZE <luxianze98@gmail.com>
2020-10-29 16:25:14 +0800

    Move this to config file Fixes #5

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import {customers} from './mock_db/mock_customer.js';
+import pkg from 'dotenv';
+
+const { config } = pkg;
+config()
 
 const app = express();
-const port = 3000 //TODO@LUXIANZE: #5 Move this to config file 
+//TODO@LUXIANZE: #5 Move this to config file 
 
 app.get('/', (req, res) => {
     const responseData = JSON.stringify(customers);
     res.send(responseData);
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
