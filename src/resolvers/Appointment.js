4e65e5140050378c7ea43d0ed2701293cd80ec84
LUXIANZE <luxianze98@gmail.com>
2021-01-23 10:13:09 +0800

    return errors instead of throwing - To ensure correct errors thrown since we used graphql-shield

--- src/resolvers/Appointment.js
+++ src/resolvers/Appointment.js
@@ -7,19 +7,19 @@ const AppointmentResolver = {
   Query: {
     appointments: async (root, { filter }, context, info) => {
       if (!filter) {
-        throw new UserInputError("No filter provided");
+        return new UserInputError("No filter provided");
       }
       try {
         const filteredAppointments = await Appointment.find(JSON.parse(filter));
         return filteredAppointments;
       } catch (err) {
-        throw new ApolloError(err.message, 500);
+        return new ApolloError(err.message, 500);
       }
     },
     appointment: async (root, { id }, context, info) => {
       const invalid_input = id.length === 0;
       if (invalid_input) {
-        throw new UserInputError("Invalid ID number provided");
+        return new UserInputError("Invalid ID number provided");
       }
       const appointment = await Appointment.findById(id);
       return appointment;
@@ -28,14 +28,14 @@ const AppointmentResolver = {
   Mutation: {
     async createAppointment(_, { appointmentInput }) {
       if (appointmentInput === null) {
-        throw new ApolloError("Invalid input for new Appointment");
+        return new ApolloError("Invalid input for new Appointment");
       }
       const appointment = await Appointment.create({ ...appointmentInput });
       return appointment;
     },
     async updateAppointment(_, { appointmentInput }) {
       if (!appointmentInput.id) {
-        throw new UserInputError(
+        return new UserInputError(
           "Unable to update Appointment with invalid id"
         );
       }
@@ -44,32 +44,32 @@ const AppointmentResolver = {
         if (oldAppointment) {
           const CustomerID = await User.findById(appointmentInput.CustomerID);
           if (!CustomerID) {
-            throw new UserInputError("Customer ID does not exist");
+            return new UserInputError("Customer ID does not exist");
           }
           const BranchID = await Branch.findById(appointmentInput.BranchID);
           if (!BranchID) {
-            throw new UserInputError("Branch ID does not exist");
+            return new UserInputError("Branch ID does not exist");
           }
           // TO DO: Add service data
           // const ServiceID = await Service.findById(appointmentInput.ServiceID);
           // if(!ServiceID){
-          //   throw new UserInputError("Service ID does not exist")
+          //   return new UserInputError("Service ID does not exist")
           // }
           if (
             appointmentInput.AppointmentStatus ===
               APPOINTMENT_STATUS.CANCELLED ||
             appointmentInput.AppointmentStatus === APPOINTMENT_STATUS.REJECTED
           ) {
-            throw new UserInputError(
+            return new UserInputError(
               "Cancelled/Rejected Appointment cannot be updated."
             );
           }
           // Unable to check if VehicleID exist or not (NO)
           if (appointmentInput.VehicleID === null) {
-            throw new UserInputError("Vehicle ID is null");
+            return new UserInputError("Vehicle ID is null");
           }
         } else {
-          throw new UserInputError("Appointment is not found with id");
+          return new UserInputError("Appointment is not found with id");
         }
         let appointment = await Appointment.findByIdAndUpdate(
           appointmentInput.id,
@@ -80,12 +80,12 @@ const AppointmentResolver = {
         );
         return appointment;
       } catch (err) {
-        throw new ApolloError(err.message, 500);
+        return new ApolloError(err.message, 500);
       }
     },
     async deleteAppointment(_, { id }) {
       if (!id) {
-        throw new UserInputError("No id is provided");
+        return new UserInputError("No id is provided");
       } else {
         try {
           const appointment = await Appointment.findById(id);
@@ -94,7 +94,7 @@ const AppointmentResolver = {
             return deleted;
           }
         } catch (err) {
-          throw new ApolloError(err.message, 500);
+          return new ApolloError(err.message, 500);
         }
       }
     },
